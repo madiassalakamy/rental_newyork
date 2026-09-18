a8effc11d43b00c41c10a402067679710aa14403
madiassalakamyd <madiassalakamyd@gmail.com>
2026-09-18 16:17:22 +0200

    26M10_2SP3_TE_CDATA-1234_12_13_17

--- models/stg/stg_table.sql
+++ models/stg/stg_table.sql
@@ -9,5 +9,5 @@ when gender=1 then 'homme'
 when gender=2 then 'femme' 
 else 'inconnu'
 end as genre,
-count(1) over(partition by gender) as nbregender
+count(1) over(partition by gender) as nbre_gender
 from raw_newyork
